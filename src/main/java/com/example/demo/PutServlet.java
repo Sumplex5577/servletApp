a28f40aaa6c154382b3c42434459c6af681d9283
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-12 11:25:58 +0300

    Adding another parameters to the table

--- src/main/java/com/example/demo/PutServlet.java
+++ src/main/java/com/example/demo/PutServlet.java
@@ -22,12 +22,20 @@ public class PutServlet extends HttpServlet {
 
         String name = request.getParameter("name");
         String email = request.getParameter("email");
+        String country = request.getParameter("country");
+        String telnumber = request.getParameter("telnumber");
+        String surname = request.getParameter("surname");
+        String age = request.getParameter("age");
+
 
         Employee employee = new Employee();
         employee.setId(id);
         employee.setName(name);
         employee.setEmail(email);
-        employee.setCountry(request.getParameter("country"));
+        employee.setCountry(country);
+        employee.setTelnumber(telnumber);
+        employee.setSurname(surname);
+        employee.setAge(age);
 
         int status = EmployeeRepository.update(employee);
 
